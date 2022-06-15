b1e1c26cee7e6de963764d83a0d3a9faab0549e1
agudelo0103 <crisagudavid@gmail.com>
2022-06-14 20:38:55 -0500

    Actualizacion

--- src/users.js
+++ src/users.js
@@ -1,41 +1,55 @@
-const express = require("express")
+const express = require("express");
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const conection = require('./database');
 
 router.get('/', (req, res) => {
   const { user_name, password } = req.body
+
   if ((user_name && password) && (user_name.length > 0 && password.length > 0)) {
-    conection.query(`SELECT user_id FROM usuarios WHERE user_name = '${user_name}' AND password = '${password}'`, (err, rows) => {
+    conection.query(`SELECT user_id FROM users WHERE user_name = '${user_name}' AND password = '${password}'`, (err, rows) => {
       if (!err) {
         if (rows.length > 0) {
-          res.json('ya estás registrado');
+          jwt.sign({user_name, password}, 'secretKey', (err, token) => {
+            if (err) res.json('Ocurrio un error');
+            res.json({
+              message: 'Logueado correctamente',
+              token
+            })
+          });
         } else {
-          res.json('credenciales incorrectas, por favor verifique');
+          res.json('Credenciales incorrectas, por favor verifique');
         }
       } else {
         console.log(err);
       }
     });
   } else {
-    res.json('ingresa todos los datos');
+    res.json('Ingresa todos los datos');
   }
 });
 
 router.post('/', (req, res) => {
   const { user_name, password } = req.body
+
   if ((user_name && password) && (user_name.length > 0 && password.length > 0)) {
-    conection.query(`INSERT INTO usuarios VALUES (null, '${user_name}', '${password}')`, (err) => {
-      if (!err) {
-        res.json({status: 'usuario creado correctamente'});
+    conection.query(`INSERT INTO users VALUES (null, '${user_name}', '${password}')`, (err2) => {
+      if (!err2) {
+        jwt.sign({user_name, password}, 'secretKey', (err, token) => {
+          if (err) res.json('Ocurrio un error');
+          res.json({
+            message: 'Usuario creado correctamente',
+            token
+          })
+        });
       } else {
-        if (err.sqlState == 23000) {
+        if (err2.sqlState == 23000) {
           res.json('Este usuario ya fue creado, por favor intenta con otro usuario');
         }
-        console.log(err);
       }
     });
-  }
+  }  
 });
 
 module.exports = router;
